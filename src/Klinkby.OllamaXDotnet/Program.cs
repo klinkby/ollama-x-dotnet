e7a08d8861de5ddd08547659847c70da7332c370
Mads Breusch Klinkby <1681112+klinkby@users.noreply.github.com>
2024-11-13 17:54:35 +0100

    switch to gemma

--- src/Klinkby.OllamaXDotnet/Program.cs
+++ src/Klinkby.OllamaXDotnet/Program.cs
@@ -2,7 +2,7 @@
 
 const string input = "So, what is the airspeed velocity of an unladen swallow?";
 
-IChatClient chatClient = new OllamaChatClient(new Uri("http://localhost:11434"), "llama3.2:1b");
+IChatClient chatClient = new OllamaChatClient(new Uri("http://localhost:11434"), "gemma:2b");
 var stream = chatClient.CompleteStreamingAsync(
     input,
     options: new ChatOptions 
